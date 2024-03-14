9653cf8acd105f135c944cccdb66c53d63890068
stas <s_anufriev@list.ru>
2024-03-14 18:02:40 +0700

    update arr format and refactor

--- dev.js
+++ dev.js
@@ -60,128 +60,507 @@ function auto_check_iter(){
     checkDiscreteOutputs();
 }
 var arr_d_in = {
-    "in0": dev[""],
-    "in1": dev[""],
-    "in2": dev[""],
-    "in3": dev[""],
-    "in4": dev[""],
-    "in5": dev[""],
-    "in6": dev[""],
-    "in7": dev[""],
-    "in8": dev[""], // темпер корпуса горелки №1
-    "in9": dev[""],
-    "in10": dev[""],
-    "in11": dev[""], // темпер корпуса горелки №2
-    "in12": dev[""],
-    "in13": dev[""],
-    "in14": dev[""], // темпер корпуса горелки №3
-    "in15": dev[""],
-    "in16": dev[""],
-    "in17": dev[""]
+    "in0": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "включен автоматический режим"
+        }, 
+    "in1": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "включен вентилятор 1"
+        }, 
+    "in2": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "включен вентилятор 2"
+        }, 
+    "in3": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "включен вентилятор 3"
+        }, 
+    "in4": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "включен двигатель загрузочного шнека"
+        }, 
+    "in5": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "включен двигатель выгрузки"
+        }, 
+    "in6": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "включены разгрузочные вальцы"
+        }, 
+    "in7": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "испаритель горелки 1 перегрет"
+        }, 
+    "in8": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "корпус горелки 1 перегрет"
+        }, // темпер корпуса горелки №1
+    "in9": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "в корпусе горелки 1 есть пламя"
+        }, 
+    "in10": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "испаритель горелки 2 перегрет"
+        }, 
+    "in11": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "корпус горелки 2 перегрет"
+        }, // темпер корпуса горелки №2
+    "in12": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "в корпусе горелки 2 есть пламя"
+        }, 
+    "in13": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "испаритель горелки 3 перегрет"
+        }, 
+    "in14": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "корпус горелки 3 перегрет"
+        }, // темпер корпуса горелки №3
+    "in15": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "в корпусе горелки 3 есть пламя"
+        }, 
+    "in16": {
+            "mqtt": "",
+            "prior": 3,
+            "msg_true": "резерв"
+        }, 
+    "in17": {
+            "mqtt": "",
+            "prior": 3,
+            "msg_true": "резерв"
+        }  
 }
 
 var arr_d_out = {
-    "out0": dev[""],
-    "out1": dev[""],
-    "out2": dev[""],
-    "out3": dev[""],
-    "out4": dev[""],
-    "out5": dev[""],
-    "out6": dev[""],
-    "out7": dev[""],
-    "out8": dev[""],
-    "out9": dev[""],
-    "out10": dev[""],
-    "out11": dev[""],
-    "out12": dev[""],
+    "out0": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "включение вентилятора 1"
+        }, 
+    "out1": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "включение горелки 1"
+        }, 
+    "out2": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "включение вентилятора 2"
+        }, 
+    "out3": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "включение горелки 2"
+        }, 
+    "out4": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "включение вентилятора 3"
+        }, 
+    "out5": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "включение горелки 3"
+        }, 
+    "out6": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "включение двигателя загрузочного шнека"
+        }, 
+    "out7": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "выключение двигателя выгрузки"
+        }, 
+    "out8": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "включение разгрузочных вальцов"
+        }, 
+    "out9": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "включить газовый клапан"
+        }, 
+    "out10": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "включить освещение"
+        }, 
+    "out11": {
+            "mqtt": "",
+            "prior": 3,
+            "msg_true": "резерв"
+        }
 }
 
 // сигналы модуля расширения дискрет вход/выход
 var arr_d_in_ext = {
-    "in0": dev[""],
-    "in1": dev[""],
-    "in2": dev[""],
-    "in3": dev[""],
-    "in4": dev[""],
-    "in5": dev[""],
-    "in6": dev[""],
-    "in7": dev[""],
-    "in8": dev[""],
-    "in9": dev[""],
-    "in10": dev[""],
-    "in11": dev[""],
-    "in12": dev[""],
-    "in13": dev[""],
-    "in14": dev[""],
-    "in15": dev[""],
-    "in16": dev[""],
+    "in0": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""// TODO NONE
+        }, 
+    "in1": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "дверь закрыта"
+        }, 
+    "in2": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "загрузочный шнек в работе"
+        }, 
+    "in3": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "левая колонна 1 не перегрета"
+        }, 
+    "in4": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "правая колонна 1 не перегрета"
+        }, 
+    "in5": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "левая колонна 2 не перегрета"
+        }, 
+    "in6": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "правая колонна 2 не перегрета"
+        }, 
+    "in7": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "дозирующие вальцы в работе"
+        }, 
+    "in8": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "сушилка заполнена"
+        }, 
+    "in9": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "зерно есть в сушилке"
+        }, 
+    "in10": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "нет перегрева в камере 1"
+        }, 
+    "in11": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "нет перегрева в камере 2"
+        }, 
+    "in12": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "нет перегрева в камере 3"
+        }, 
+    "in13": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "давление в камере 1 есть"
+        }, 
+    "in14": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "давление в камере 2 есть"
+        }, 
+    "in15": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "давление в камере 3 есть"
+        }, 
+    "in16": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "авария питания, проблемы в сети"
+        }, 
 }
 
 var arr_d_out_ext = {
-    "out0": dev[""],
-    "out1": dev[""],
-    "out2": dev[""],
-    "out2": dev[""],
-    "out4": dev[""],
+    "out0": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "включение автомата горения 1"
+        }, 
+    "out1": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "включение автомата горения 2"
+        }, 
+    "out2": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": "включение автомата горения 3"
+        }, 
+    "out2": {
+            "mqtt": "",
+            "prior": 3,
+            "msg_true": "резерв"
+        }, 
+    "out4": {
+            "mqtt": "",
+            "prior": 3,
+            "msg_true": "резерв"
+        }, 
 }
 
 
 // сигналы модуля расширения дискрет вход/выход
 var arr_d_in_ext = {
-    "in0": dev[""],
-    "in1": dev[""],
-    "in2": dev[""],
-    "in3": dev[""],
-    "in4": dev[""],
-    "in5": dev[""],
-    "in6": dev[""],
-    "in7": dev[""],
-    "in8": dev[""],
-    "in9": dev[""],
-    "in10": dev[""],
-    "in11": dev[""],
-    "in12": dev[""],
-    "in13": dev[""],
-    "in14": dev[""],
-    "in15": dev[""],
-    "in16": dev[""],
+    "in0": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "in1": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "in2": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "in3": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "in4": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "in5": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "in6": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "in7": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "in8": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "in9": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "in10": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "in11": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "in12": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "in13": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "in14": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "in15": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "in16": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
 }
 
 var arr_d_out_ext = {
-    "out0": dev[""],
-    "out1": dev[""],
-    "out2": dev[""],
-    "out3": dev[""],
-    "out4": dev[""],
+    "out0": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "out1": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "out2": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "out3": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "out4": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
 }
 
 var arr_a_in = {
-    "in0": dev[""],
-    "in1": dev[""],
-    "in2": dev[""],
-    "in3": dev[""],
-    "in4": dev[""],
-    "in5": dev[""],
-    "in6": dev[""],
-    "in7": dev[""],
+    "in0": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "in1": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "in2": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "in3": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "in4": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "in5": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "in6": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "in7": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
 }
 
 var arr_a_out = {
-    "out0": dev[""],
-    "out1": dev[""],
-    "out2": dev[""],
-    "out3": dev[""],
-    "out4": dev[""],
-    "out5": dev["system/Reboot"],
-    "out6": dev["system/Reboot"],
-    "out7": dev["metrics/ram_used"],
-    "out8": dev["wb-gpio/A1_OUT"],
-    "out9": dev["buzzer/volume"],
-    "out10": dev["wb-adc/A3"],
-    "out11": dev["knx/data"],
-    "out12": dev[""],
+    "out0": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "out1": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "out2": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "out3": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "out4": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "out5": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "out6": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "out7": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "out8": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "out9": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "out10": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "out11": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
+    "out12": {
+            "mqtt": "",
+            "prior": 1,
+            "msg_true": ""
+        }, 
 }
-
+//TODO 3 уровня важности(1-все стоп; 2-предупреждение; 3-только запись в лог)
 var data_modules = {
        "arr_d_in": arr_d_in,
        "arr_d_out": arr_d_out,
@@ -195,10 +574,12 @@ function init_modules(){
        for (var key in data_modules[mod_type]){
            try {
                // TODO сделать проверку иначе (не вызывая в консоль)
-                send_log("[+] - " + mod_type + " -> " + key + " - " + data_modules[mod_type][key]);
+                var topic = data_modules[mod_type][key]["mqtt"];
+                var value = dev[topic];
+                send_log("[+] - " + mod_type + " -> " + key + " - " + value);
            } catch (error) {
                 if (error instanceof TypeError){
-                    fault_handling(true, "[-] - " + mod_type + " -> " + key);
+                    fault_handling(true, "[-] - " + mod_type + " -> " + topic);
                 } else {
                     fault_handling(true, "[!] - " + error);
                 }
@@ -274,7 +655,30 @@ function checkDiscreteOutputs() {
     log("Свободный дискретный выход контроллера");
   }
  }
-
+function checkDiscreteInputs() {
+    
+    for (var mod_type in data_modules){
+           for (var key in data_modules[mod_type]){
+               try {
+                    // TODO сделать проверку иначе (не вызывая в консоль)
+                    var topic = data_modules[mod_type][key]["mqtt"];
+                    var priority = data_modules[mod_type][key]["prior"];
+                    var msg_true = data_modules[mod_type][key]["msg_true"];
+                    var value = dev[topic];
+                    check_module(topic, priority, msg_true);
+                   //send_log("[+] - " + mod_type + " -> " + key + " - " + value);
+                } catch (error) {
+                    // TODO возможно отказаться от проверки на 
+                    // подключение модулей в начале главной итерации
+                    if (error instanceof TypeError){
+                        fault_handling(true, "[-] - " + mod_type + " -> " + topic);
+                    } else {
+                        fault_handling(true, "[!] - " + error);
+                    }
+                }
+            } 
+    }
+}
 function checkDiscreteInputs() {
   // проверка дискретных входов
   // Контроль режима. Наличие сигнала говорит о том, что включен автоматический режим.
